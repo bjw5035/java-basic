eeeeba4118ff9790ed89c97c99e0c51d0afc110b
Jae Woo <bjw5035@gmail.com>
2024-08-30 18:15:45 +0900

    feat : 실전 자바 NullPointExecption, 문제풀이 진행중

---
+++ src/ref/ex/ProductOrderMain2.java
@@ -0,0 +1,21 @@
+package ref.ex;
+
+public class ProductOrderMain2 {
+
+    public static void main(String[] args) {
+        // 여러 상품의 주문 정보를 담는 배열 생성
+        ProductOrder[] productOrders = new ProductOrder[3];
+
+        // createOrder()를 여러번 사용해서 상품 주문 정보들을 생성하고 배열에 저장
+
+        // printOrders()를 사용해서 상품 주문 정보 출력
+        // getTotalAmount()를 사용해서 총 결제 금액 계산
+        // 총 결제 금액 출력
+
+    }
+
+    static void createOrder() {
+
+    }
+
+}
