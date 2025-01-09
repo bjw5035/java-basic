ffbf66dad696ecef2404359de36edf3f84e46e2e
Jae Woo <bjw5035@gmail.com>
2025-01-09 17:02:49 +0900

    feat : 실전 자바 객체지향프로그래밍 (다형성 진행중)

--- src/poly/basic/PolyMain.java
+++ src/poly/basic/PolyMain.java
@@ -8,7 +8,22 @@ public class PolyMain {
         System.out.println("Parent -> Parent");
         Parent p = new Parent();
         p.parentMethod();
-//        System.out.println("Child -> Child");
+
+        // 자식 변수가 자식 인스턴스 참조
+        System.out.println("Child -> Child");
+        Child child = new Child();
+        child.parentMethod();
+        child.childMethod();
+
+        // 부모 변수가 자식 인스턴스 참조(다형적 참조)
+        System.out.println("Parent -> Child");
+        Parent poly = new Child();
+        poly.parentMethod();
+
+        // Child child1 = new Parent(); -> 자식은 부모를 담을 수 없다.
+
+        // 자식의 기능은 호출할 수 없다. 컴파일 오류
+        // poly.childMethod();
     }
 
 }
